13c8e1372dad77a38f1a5578e112ac996f8249e7
Théophile Bastian <contact@tobast.fr>
2016-01-05 16:27:16 +0100

    Added flags position on the flags wire

--- processor/constants.py
+++ processor/constants.py
@@ -1,14 +1,19 @@
 """ Contains the constants used all around the project """
 
-OPCODE_FRAME = {
-		conditionnal = 1,
-		instuction = 5,
-		writeResult = 9,
-		useCarry = 10,
-		setFlags = 11,
-		destRegister = 12,
-		op1 = 16,
-		isOp1Filled = 20,
-		op2 = 21,
-		memAddr = 46
-	}
+class OPCODE_FRAME :
+	conditionnal = 1
+	instuction = 5
+	writeResult = 9
+	useCarry = 10
+	setFlags = 11
+	destRegister = 12
+	op1 = 16
+	isOp1Filled = 20
+	op2 = 21
+	memAddr = 46
+
+class FLAGS_POSITION:
+	N = 1
+	Z = 2
+	C = 3
+	V = 4
