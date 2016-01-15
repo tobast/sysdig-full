68394d9e9e6f5fc01517124fffa8411787cd7a43
Novatux <nathanael.courant@laposte.net>
2016-01-15 16:47:17 +0100

    Fix memory

--- processor/memory_unit.py
+++ processor/memory_unit.py
@@ -9,6 +9,7 @@ def memory_unit(instr, value, addr, result = None):
 	we = nl.AND(is_memory, nl.SELECT(3, instr))
 	addr = nl.SLICE(1, 16, addr)
 	if UNUSED_ADDR_ZERO:
-            addr = nl.AND(addr, helpers.wire_expand(16, is_memory))
-	return nl.RAM(16, 64, addr, we, addr, value, result)
+		addr = nl.AND(addr, helpers.wire_expand(16, is_memory))
+	uu = nl.fresh(64)
+	return nl.RAM(16, 64, addr, we, nl.WIRE(addr, uu), value, result)
 	
