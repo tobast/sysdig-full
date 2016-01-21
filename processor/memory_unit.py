f46dcb55678852dcee065081c10ee8dc72d3e097
Ekdohibs <nathanael.courant@laposte.net>
2016-01-21 10:42:32 +0100

    Add memory unit test, fix memory

--- processor/memory_unit.py
+++ processor/memory_unit.py
@@ -7,7 +7,7 @@ UNUSED_ADDR_ZERO = True
 def memory_unit(instr, value, addr, result = None):
 	nl.push_context("memory_unit")
 	is_memory = nl.AND(nl.SELECT(1, instr), nl.SELECT(4, instr))
-	we = nl.AND(is_memory, nl.SELECT(3, instr))
+	we = nl.AND(is_memory, nl.SELECT(2, instr))
 	addr = nl.SLICE(1, 16, addr)
 	if UNUSED_ADDR_ZERO:
 		addr = nl.AND(addr, helpers.wire_expand(16, is_memory))
