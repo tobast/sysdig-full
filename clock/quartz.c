911389300b97898ebef06e6670381d0b46a3dcaf
Théophile Bastian <contact@tobast.fr>
2016-01-23 11:48:38 +0100

    Added new quartz -> double the perfs!

---
+++ clock/quartz.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <time.h>
+
+void print_int64(unsigned long long int val) {
+	for(int byte=7; byte >=0; byte--)
+		putchar((char)(val >> (byte*8)));
+}
+
+int main() {
+	time_t rawTime;
+	time(&rawTime);
+	struct tm* curTime = localtime(&rawTime);
+	curTime->tm_year += 1900;
+	curTime->tm_mon ++;
+
+	print_int64(curTime->tm_sec);
+	print_int64(curTime->tm_min);
+	print_int64(curTime->tm_hour);
+	print_int64(curTime->tm_mday);
+	print_int64(curTime->tm_mon);
+	print_int64(curTime->tm_year % 100);
+	print_int64((curTime->tm_year / 100) - 10);
+
+	while(1) {
+		putchar(0);
+		putchar(0);
+		putchar(0);
+		putchar(0);
+		putchar(0);
+		putchar(0);
+		putchar(0);
+		putchar(0);
+		putchar(0);
+		putchar(0);
+		putchar(0);
+		putchar(0);
+		putchar(0);
+		putchar(0);
+		putchar(0);
+		putchar(0);
+	}
+
+	return 0;
+}
+
